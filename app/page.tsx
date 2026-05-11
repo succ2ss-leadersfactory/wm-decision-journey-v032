c1e618bbaa53e3279b4f7797976f4be7fe061ef4
succ2ss-leadersfactory <succ2ss@gmail.com>
2026-05-11 11:25:50 +0900

    Use CHA Bio Group branded learner app v13

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
-import LearnerAppV12 from "@/components/LearnerAppV12";
+import LearnerAppV13 from "@/components/LearnerAppV13";
 
 export default function Page() {
   return (
     <main className="flex min-h-screen items-center justify-center bg-gradient-to-br from-slate-50 via-blue-50/30 to-teal-50/30 p-6">
-      <LearnerAppV12 />
+      <LearnerAppV13 />
     </main>
   );
 }
